fa9ee30f5d53d7f41f33adeee115bebb49950ab5
Andrey Guzev <kodermanishe@gmail.com>
2019-05-14 20:45:35 -0500

    bugs

--- ModulesandClosureLab_Week3_Tuesday/1/scr.js
+++ ModulesandClosureLab_Week3_Tuesday/1/scr.js
@@ -1,13 +1,18 @@
-timer = null;
 
-function rudy() {
-    document.getElementById("output").innerHTML += "Rudy!";
-}
-var rudyTimer = (function() {
-    if (timer === null) {
-        timer = setInterval(rudy, 1000);
-    } else {
-        clearInterval(timer);
-        timer = null;
+var timer = null;
+
+var rudyTimer = (function () {
+    rudy = function(){
+        document.getElementById("output").innerHTML += "Rudy!";
+    }
+    return {
+        execute: function () {
+            if (timer == null) {
+                this.timer = setInterval(rudy, 1000);
+            } else {
+                clearInterval(timer);
+                timer = null;
+            }
+        }
     }
 })();
--- ModulesandClosureLab_Week3_Tuesday/2/scr.js
+++ ModulesandClosureLab_Week3_Tuesday/2/scr.js
@@ -12,26 +12,28 @@ var account = {
         return "Account name: " + this.accountName + ": " + this.deposit;
     }
 };
-var accountAdd = {
-    addNewAccount: function(name, depo){
-        let acc = Object.create(account);
-        acc.accountName = name;
-        acc.deposit = depo;
-        if (accounts == null)
-            accounts = [acc];
-        else
-            accounts[accounts.length] = acc;
-    },
-    updateText: function(){
-        var text = "";
-        for (var i = 0; i < accounts.length; i++){
-            if (text != "")
-                text+="\n";
-            text+=accounts[i].toStr();
+var accountAdd = (function () {
+    return {
+        addNewAccount: function (name, depo) {
+            let acc = Object.create(account);
+            acc.accountName = name;
+            acc.deposit = depo;
+            if (accounts == null)
+                accounts = [acc];
+            else
+                accounts[accounts.length] = acc;
+        },
+        updateText: function () {
+            var text = "";
+            for (var i = 0; i < accounts.length; i++) {
+                if (text != "")
+                    text += "\n";
+                text += accounts[i].toStr();
+            }
+            document.getElementById("text").value = text;
         }
-        document.getElementById("text").value = text;
     }
-};
+})();
 function addAccount(){
     let name = document.getElementById("name").value;
     let dep = document.getElementById("depo").value;
